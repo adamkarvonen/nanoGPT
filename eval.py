14febaf56a11cf71b21cbf428e6457f9e8532059
Adam Karvonen <adam.karvonen@gmail.com>
2023-04-02 17:16:25 -0500

    Move training data into data folder, add eval script

--- eval.py
+++ eval.py
@@ -13,11 +13,13 @@ init_from = (
     "resume"  # either 'resume' (from an out_dir) or a gpt2 variant (e.g. 'gpt2-xl')
 )
 out_dir = "out"  # ignored if init_from is not 'resume'
-start = "1+1="  # or "<|endoftext|>" or etc. Can also specify a file, use as: "FILE:prompt.txt"
-num_samples = 10  # number of samples to draw
-max_new_tokens = 500  # number of tokens generated in each sample
+input_dir = "addition"
+test_name = "test.txt"
+start = "12+44="  # or "<|endoftext|>" or etc. Can also specify a file, use as: "FILE:prompt.txt"
+num_samples = 1  # number of samples to draw
+max_new_tokens = 6  # number of tokens generated in each sample
 temperature = (
-    0.8  # 1.0 = no change, < 1.0 = less random, > 1.0 = more random, in predictions
+    0.01  # 1.0 = no change, < 1.0 = less random, > 1.0 = more random, in predictions
 )
 top_k = (
     200  # retain only the top_k most likely tokens, clamp others to have 0 probability
@@ -28,6 +30,9 @@ dtype = "bfloat16"  # 'float32' or 'bfloat16' or 'float16'
 compile = False  # use PyTorch 2.0 to compile the model to be faster
 exec(open("configurator.py").read())  # overrides from command line or config file
 # -----------------------------------------------------------------------------
+
+test_input = "data/" + input_dir + "/" + test_name
+test_output = "data/" + input_dir + "/eval.txt"
 
 torch.manual_seed(seed)
 torch.cuda.manual_seed(seed)
@@ -92,16 +97,65 @@ else:
     decode = lambda l: enc.decode(l)
 
 # encode the beginning of the prompt
-if start.startswith("FILE:"):
-    with open(start[5:], "r", encoding="utf-8") as f:
-        start = f.read()
-start_ids = encode(start)
-x = torch.tensor(start_ids, dtype=torch.long, device=device)[None, ...]
+# if start.startswith("FILE:"):
+#     with open(start[5:], "r", encoding="utf-8") as f:
+#         start = f.read()
+num_correct = 0
+total = 0
+mistakes = []
+with open(test_input, "r") as f:
+    for line in f:
+        # making sure addition is correct in case I insert wrong answers into training data
+        split_str = line.split("+")
+        first_num = int(split_str[0])
+        second_num = int(split_str[1].split("=")[0])
+        ans = first_num + second_num
+        start_ids = encode(f"{first_num}+{second_num}=")
+
+        x = torch.tensor(start_ids, dtype=torch.long, device=device)[None, ...]
+        with torch.no_grad():
+            with ctx:
+                for k in range(num_samples):
+                    y = model.generate(
+                        x, max_new_tokens, temperature=temperature, top_k=top_k
+                    )
+
+                    model_response = decode(y[0].tolist())
+                    if first_num == 19 and second_num == 74:
+                        print(start_ids)
+                        print(y)
+                        print(model_response)
+                    # print(model_response)
+                    # prevents errors if model returns answer that is in the wrong format
+                    model_ans = -1
+                    try:
+                        model_ans = int(model_response.split(";")[0].split("=")[1])
+                    except:
+                        print("error in model response", model_response)
+                    total += 1
+
+                    if model_ans == ans:
+                        # print("Correct")
+                        num_correct += 1
+                    else:
+                        mistakes.append(model_response)
+                    # print("---------------")
+
+print(num_correct, "correct out of", total)
+
+with open(test_output, "a") as f:
+    f.write(str(num_correct) + " correct out of " + str(total) + "\n")
+    for mistake in mistakes:
+        f.write(mistake.strip() + "\n")
+    f.write("\n")
+
+# start_ids = encode(start)
+# x = torch.tensor(start_ids, dtype=torch.long, device=device)[None, ...]
 
 # run generation
-with torch.no_grad():
-    with ctx:
-        for k in range(num_samples):
-            y = model.generate(x, max_new_tokens, temperature=temperature, top_k=top_k)
-            print(decode(y[0].tolist()))
-            print("---------------")
+# with torch.no_grad():
+#     with ctx:
+#         for k in range(num_samples):
+#             y = model.generate(x, max_new_tokens, temperature=temperature, top_k=top_k)
+#             print(decode(y[0].tolist()))
+#             print("---------------")
